34fe9b9c8fe5e20b0ac8c1417d29fba028e8b3f9
Amira Yasser <72684587+amirayasser@users.noreply.github.com>
2024-05-14 06:35:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Using the Unary Plus (+) Operator before string variable
+let x = '4';
+console.log(+x); //4
+console.log(2 + +x); //6
+console.log(Number(x)); // 4
+console.log(parseInt(x)); // 4
+
+// Using the built-in Number Constructor Function (The most direct way) 
+const str = "10";
+const number = Number(str);
+console.log(number); // 10
+
+// also you can use parseInt orr parseFloat
